2f2d6b3cd2886adb28ceeee401c17c6eae52612d
박선영 <sunny.playgnd@gmail.com>
2023-06-02 14:15:33 +0900

    이미지 링크 수정

--- src/components/search_header/search_header.jsx
+++ src/components/search_header/search_header.jsx
@@ -24,10 +24,10 @@ const SearchHeader = memo((props) => {
 
                     <div className={styles.start}>
                         <button className={styles.hamburger}>
-                                <img className={styles.hamburgerImg}src="/images/hamburger.png" alt="hamburger" />        
+                                <img className={styles.hamburgerImg}src="images/hamburger.png" alt="hamburger" />        
                         </button>
                         <span className={styles.youtube} onClick={props.onLogoClick}>
-                        <img className={styles.logo} src="/images/logo.png" alt="logo"/>
+                        <img className={styles.logo} src="images/logo.png" alt="logo"/>
                         <h1 className={styles.title} >YouTube</h1>
                         </span>
                     </div>
@@ -35,17 +35,17 @@ const SearchHeader = memo((props) => {
                     <div className={styles.center}>
                         <input ref={inputRef} className={styles.input} type="search" placeholder="검색" onKeyPress={onKeyPress}/>
                         <button className={styles.searchBtn} type="submit" onClick={onClick}>
-                                <img className={styles.searchIcon}src="/images/search.png" alt="search" />
+                                <img className={styles.searchIcon}src="images/search.png" alt="search" />
                         </button>
-                        <img className={styles.microphoneImg}src="/images/microphone.png" alt="microphone" />
+                        <img className={styles.microphoneImg}src="images/microphone.png" alt="microphone" />
                     </div>
 
                     <div className={styles.end}>
-                        <img className={styles.searchImg}src="/images/search.png" alt="searchImg" />
-                        <img className={styles.videoImg}src="/images/video.png" alt="video" />
-                        <img className={styles.app_menuImg}src="/images/app_menu.png" alt="app_menu" />
-                        <img className={styles.bellImg}src="/images/bell.png" alt="bell" />
-                        <img className={styles.profileImg}src="/images/profile.png" alt="profile" />
+                        <img className={styles.searchImg}src="images/search.png" alt="searchImg" />
+                        <img className={styles.videoImg}src="images/video.png" alt="video" />
+                        <img className={styles.app_menuImg}src="images/app_menu.png" alt="app_menu" />
+                        <img className={styles.bellImg}src="images/bell.png" alt="bell" />
+                        <img className={styles.profileImg}src="images/profile.png" alt="profile" />
                     </div>
                     
                 </header>
